8c38d12d8a74b47913f7f01e547d775ff48140b3
Benjamin <112658491+Benjamingavriely@users.noreply.github.com>
2024-03-29 16:34:19 -0400

    initialised start screen

--- startScreen.py
+++ startScreen.py
@@ -1,1 +1,8 @@
 import pygame
+
+def run_start(screen: pygame.display) -> None:
+    """
+    This displays the start screen. This will include a title, start button,
+    game mode, and quit button.
+    """
+    
