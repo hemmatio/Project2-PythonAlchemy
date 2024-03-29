1f5005dab58fc866afc49844091ceb0cb0e135ff
Benjamin <112658491+Benjamingavriely@users.noreply.github.com>
2024-03-29 16:18:07 -0400

    Update run.py

---
+++ run.py
@@ -0,0 +1,18 @@
+import pygame
+pygame.init()
+
+# Set up the drawing window
+screen = pygame.display.set_mode([500, 500])
+
+# Run until the user asks to quit
+running = True
+while running:
+
+    # Did the user click the window close button?
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+
+# Done! Time to quit.
+pygame.quit()
