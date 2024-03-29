f96c6677f3cff5329db4235d09043672d4f29e16
Benjamin <112658491+Benjamingavriely@users.noreply.github.com>
2024-03-29 16:29:36 -0400

    for neyl

--- run.py
+++ run.py
@@ -1,8 +1,9 @@
 import pygame
+import startScreen
 pygame.init()
 
 # Set up the drawing window
-screen = pygame.display.set_mode([500, 500])
+screen = pygame.display.set_mode([1500, 800])
 
 # Run until the user asks to quit
 running = True
@@ -10,6 +11,7 @@ while running:
 
     # Did the user click the window close button?
     for event in pygame.event.get():
+
         if event.type == pygame.QUIT:
             running = False
 
